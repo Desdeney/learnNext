fcb3b3dc373fe10bafce5d962ddec856ff33dbb8
Fabian Ernst <fabian.ernst@omg.de>
2024-10-18 14:01:39 +0200

    Initial

---
+++ app/admin/page.tsx
@@ -0,0 +1,16 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+import { Card } from '../ui/dashboard/cards';
+
+
+export default function Page() {
+  return (
+    <main>
+              <p>Test dasd</p>
+    </main>
+
+)}
+
